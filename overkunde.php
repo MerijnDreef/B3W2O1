f094e299420d4feccfcc59df95862523fe8a0593
Merijn <99054535@mydavinci.nl>
2020-02-18 14:14:58 +0100

    HTML pagina's klaar nu nog php

--- overkunde.php
+++ overkunde.php
@@ -8,9 +8,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="opdracht.css">
-    <title>Een opdracht</title>
+    <link href="https://fonts.googleapis.com/css?family=Lacquer&display=swap" rel="stylesheet">
+    <title>Onkunde</title>
 </head>
 <body>
-    
+<h1 id="logo">Mad Libs</h1>
+    <ul class="menu">
+        <li><a href="index.html">Er heerst paniek...</a></li>
+        <li><a href="onkunde.html">Onkunde</a></li>
+    </ul>
+    <div class="backgroundForm">
+        <h1 id="keuzeh1">Onkunde</h1>
+        
+    </div>
+    <p id="footer">Deze website is gemaakt door &copy; Merijn Dreef, 2020</p>
 </body>
 </html>
