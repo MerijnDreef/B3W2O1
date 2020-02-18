0bee4360031d04303fa0166fb77f9f65f6e9e6d1
Merijn <99054535@mydavinci.nl>
2020-02-18 14:51:09 +0100

    texten klaar nu nog voor de validatie en zulk soort dingen

--- antwoord.php
+++ antwoord.php
@@ -19,7 +19,15 @@
     </ul>
     <div class="backgroundForm">
         <h1 id="keuzeh1">Er heerst paniek...</h1>
-       
+       <p>Er heerst een paniek in het koninkrijk<?= $_POST['vraag3']?>. Koning <?= $_POST['vraag6']?> is ten einde raad en als koning <?=$_POST['vraag6']?> ten einde raad is, dan roept hij zijn ten-einde-raadsheer <?=$_POST['vraag2']?>.</p>
+       <p>"<?=$_POST['vraag2']?>! Het is een ramp! Het is een schande!"</p>
+       <p>"Sire, Majesteit, Uwe luidruchtigheid, wat is er aan de hand?"</p>
+       <p>"Mijn <?=$_POST['vraag1']?> is verdwenen! Zo maar, zonder waarschuwing. En ik had net <?=$_POST['vraag5']?> voor hem gekocht!"</p>
+       <p>"Majesteit, uw <?=$_POST['vraag1']?> komt vast vanzelf weer terug?"</p>
+       <p>"Ja, da's leuk en aardig, maar hoe moet ik in de tussentijd <?=$_POST['vraag8']?> leren?"</p>
+       <p>"Maar Sire, daar kunt u toch uw <?=$_POST['vraag7']?> voor gebruiken."</p>
+       <p>"<?=$_POST['vraag2']?>, je hebt helemaal gelijk! Wat zou ik doen als ik jou niet had."</p>
+       <p>"Mij vervelen, Sire"</p>
     </div>
     <p id="footer">Deze website is gemaakt door &copy; Merijn Dreef, 2020</p>
 </body>
--- overkunde.php
+++ overkunde.php
@@ -19,7 +19,7 @@
     </ul>
     <div class="backgroundForm">
         <h1 id="keuzeh1">Onkunde</h1>
-        
+        <p>Er zijn veel mensen die niet kunnen <?=$_POST['vraag1']?>. Neem nou <?=$_POST['vraag2']?>. Zelfs met de hulp van een <?=$_POST['vraag4']?> of zelfs <?=$_POST['vraag3']?> kan <?=$_POST['vraag2']?> niet <?=$_POST['vraag1']?>. Dar heeft niet te maken met een gebrek aan <?=$_POST['vraag5']?>, maar met een te veel aan <?=$_POST['vraag6']?>. Te veel <?=$_POST['vraag6']?> leidt tot <?=$_POST['vraag7']?> en dat is niet goed las je wilt <?=$_POST['vraag1']?>. Helaas voor <?=$_POST['vraag2']?>.</p>
     </div>
     <p id="footer">Deze website is gemaakt door &copy; Merijn Dreef, 2020</p>
 </body>
